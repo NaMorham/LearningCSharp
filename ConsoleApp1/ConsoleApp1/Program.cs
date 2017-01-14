d1a6808c17de11142c111db2f0a4befd8e014004
NaMorham <namorham@gmail.com>
2017-01-14 11:25:48 +1000

    C#Learning - 1 - First blood

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nahhhh");
+            Console.WriteLine("Got {0} arguments", args.Length);
+
+            int counter = 1;    // display counter not array index
+            foreach (String a in args)
+            {
+                Console.WriteLine("Arg [{0}/{1}]: {2:s}", counter++, args.Length, a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
