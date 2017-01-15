31ad643af68695d616e0679b613603de731da124
NaMorham <namorham@gmail.com>
2017-01-15 15:41:48 +1000

    C#Learning - 1 - Chapter 5 Exercises - done

--- ConsoleApps/CSharpBasicsS5L42Exercises/Program.cs
+++ ConsoleApps/CSharpBasicsS5L42Exercises/Program.cs
@@ -8,58 +8,9 @@ namespace CSharpBasicsS5L42Exercises
 {
     class Program
     {
-        static string getIntString(string prompt, string exitStr)
+        static public void Ex1(Exercise ex)
         {
-            while (true)
-            {
-                Console.Write(prompt.Length > 0 ? prompt : "Enter an integer: ");
-                try
-                {
-                    string val = Console.ReadLine();
-                    if (val.ToUpper() == exitStr.ToUpper())
-                    {
-                        return val;
-                    }
-                    Convert.ToInt32(val);
-                    return val;
-                }
-                catch (Exception e)
-                {
-                    Console.Error.WriteLine("Exception [{0}] getting int", e.GetType());
-                }
-            }
-        }
-
-        static public void Ex1()
-        {
-            /*
-            while (true)
-            {
-                Console.WriteLine("Enter a value or \"quit\" to quit");
-                string val = Console.ReadLine();
-                if (val.ToUpper() == "QUIT" || val.ToUpper() == "EXIT")
-                {
-                    break;
-                }
-                Console.WriteLine("Check [{0}]", val);
-                try
-                {
-                    int num = Convert.ToInt32(val);
-                    if (num >=1 && num <= 10)
-                    {
-                        Console.WriteLine("Valid");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exception parsing val [{0}] --> [{1}]", val, e.GetType());
-                }
-            }
-            /*/
+            Console.WriteLine("--------------\n{0}", ex);
             while (true)
             {
                 try
@@ -85,11 +36,11 @@ namespace CSharpBasicsS5L42Exercises
                     Console.Error.WriteLine("Exception parsing val [{0}]", e.GetType());
                 }
             }
-            //*/
         }
 
-        static public void Ex2()
+        static public void Ex2(Exercise ex)
         {
+            Console.WriteLine("\n\n--------------\n{0}", ex);
             while (true)
             {
                 try
@@ -113,19 +64,93 @@ namespace CSharpBasicsS5L42Exercises
             }
         }
 
+        static public void Ex3(Exercise ex)
+        {
+            Console.WriteLine("\n\n--------------\n{0}", ex);
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" Enter width: ");
+                    string str1 = Console.ReadLine();
+                    if (str1.ToUpper() == "QUIT")
+                        break;
+                    float width = Convert.ToSingle(str1);
+                    Console.Write("Enter length: ");
+                    string str2 = Console.ReadLine();
+                    if (str2.ToUpper() == "QUIT")
+                        break;
+                    float length = Convert.ToSingle(str2);
+                    if (width == length)
+                    {
+                        Console.WriteLine("Square");
+                    }
+                    else if (width > length)
+                    {
+                        Console.WriteLine("Landscape");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Portrait");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Exception parsing val [{0}]", e.GetType());
+                }
+            }
+        }
+
+        static public void Ex4(Exercise ex)
+        {
+            Console.WriteLine("\n\n--------------\n{0}", ex);
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" Enter limit: ");
+                    string str1 = Console.ReadLine();
+                    if (str1.ToUpper() == "QUIT")
+                        break;
+                    float speedLimit = Convert.ToSingle(str1);
+                    Console.Write("Enter speed: ");
+                    string str2 = Console.ReadLine();
+                    if (str2.ToUpper() == "QUIT")
+                        break;
+
+                    float speed = Convert.ToSingle(str2);
+                    if (speed < speedLimit) // as per spec, but wtf should be <=
+                    {
+                        Console.WriteLine("Ok");
+                    }
+                    else
+                    {
+                        double overSpeed = speed - speedLimit;
+                        int demerits = (int)(overSpeed / 5.0);
+                        Console.WriteLine("{0} demerit points{1}", demerits, demerits > 12 ? " License Suspended" : "");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Exception parsing val [{0}]", e.GetType());
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Exercise ex1 = new Exercise("Integer validation exercise",
                 "Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display \"Valid\" on the console. Otherwise, display \"Invalid\". (This logic is used a lot in applications where values entered into input boxes need to be validated.)");
-            // Console.WriteLine(ex.ToString());
-            Console.WriteLine(ex1);
-            Ex1();
+            Ex1(ex1);
 
             Exercise ex2 = new Exercise("Get max int exercise",
                 "Write a program which takes two numbers from the console and displays the maximum of the two.");
-            // Console.WriteLine(ex.ToString());
-            Console.WriteLine(ex2);
-            Ex2();
+            Ex2(ex2);
+
+            Exercise ex3 = new Exercise("Portrait or landscape", "Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.");
+            Ex3(ex3);
+
+            Ex4(new Exercise("Speed camera", "Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. If the user enters a value less than the speed limit, program should display Ok on the console. If the value is above the speed limit, the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended."));
         }
     }
 }
